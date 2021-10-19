fe43b1a942d0ec3cd67c5d4831269ca52f3526aa
NSGUF <1968083780@qq.com>
2021-10-19 20:47:32 +0800

    update: 更新测试用例

--- src/__tests__/table.spec.js
+++ src/__tests__/table.spec.js
@@ -4,15 +4,16 @@ import { TestTable } from '../table'
 describe('Table', () => {
     const TableMount = options => mount(TestTable, options)
 
+    // 渲染正常
     test('render', () => {
         const wrapper = TableMount()
-        // expect(wrapper.html()).toMatchSnapshot()
         expect(() => {
             wrapper.vm.$forceUpdate()
             wrapper.vm.$destroy()
         }).not.toThrow()
     })
 
+    // 排序功能正常,自定义sort函数
     test('click sort', async () => {
         const options = {
             propsData: {
@@ -48,17 +49,106 @@ describe('Table', () => {
 
         await wrapper.find('button.btn-def').trigger('click')
         expect(wrapper.findComponent({ name: 'TableBody' }).vm.data.map(item => item.name).join('')).toBe('cjjwe')
-
-        await wrapper.find('li.active').trigger('click')
     })
 
-    // test('props test', () => {
-    //     const wrapper = TableMount({
-    //         propsData: {
-    //             test: true,
-    //         },
-    //     })
-    //
-    //     expect(wrapper.find('.test-class').exists()).toBeTruthy()
-    // })
+    // 排序功能正常,默认sort
+    test('click myself sort', async () => {
+        const options = {
+            propsData: {
+                options: {
+                    data: [
+                        { name: 'cjj', age: 18 },
+                        { name: 'we', age: 32 },
+                        { name: 'r', age: 43 },
+                        { name: 'ew', age: 34 },
+                        { name: 'fds', age: 5 },
+                        { name: 'd', age: 43 },
+                        { name: 'f', age: 5 },
+                        { name: 'fzf', age: 23 },
+                    ],
+                    tableColumn: [
+                        { label: '名字', index: 'name' },
+                        { label: '年龄', index: 'age' , render: col => '' + col.label },
+                    ],
+                    local: true,
+                    limit: 2,
+                    showPageColNum: 2,
+                },
+            },
+        }
+        const wrapper = TableMount(options)
+        await wrapper.find('button.btn-asc').trigger('click')
+        expect(wrapper.findComponent({ name: 'TableBody' }).vm.data.map(item => item.name).join('')).toBe('cjjd')
+
+        await wrapper.find('button.btn-des').trigger('click')
+        expect(wrapper.findComponent({ name: 'TableBody' }).vm.data.map(item => item.name).join('')).toBe('wer')
+
+        await wrapper.find('button.btn-def').trigger('click')
+        expect(wrapper.findComponent({ name: 'TableBody' }).vm.data.map(item => item.name).join('')).toBe('cjjwe')
+
+    })
+
+    // 分页点击最后一页
+    test('click pagination', async () => {
+        const options = {
+            propsData: {
+                options: {
+                    data: [
+                        { name: 'cjj', age: 18 },
+                        { name: 'we', age: 32 },
+                        { name: 'r', age: 43 },
+                        { name: 'ew', age: 34 },
+                        { name: 'fds', age: 5 },
+                        { name: 'd', age: 43 },
+                        { name: 'f', age: 5 },
+                    ],
+                    tableColumn: [
+                        { label: '名字', index: 'name' },
+                        { label: '年龄', index: 'age' , render: col => '' + col.label },
+                    ],
+                    local: true,
+                    limit: 2,
+                    showPageColNum: 2,
+                },
+            },
+        }
+        const wrapper = TableMount(options)
+
+        const last = wrapper.find('li.pagine-item:last-child')
+        await last.trigger('click')
+
+        expect(wrapper.findComponent({ name: 'TableBody' }).vm.data.map(item => item.name).join('')).toBe('ffzf')
+    })
+
+    // 分页点击 ...
+    test('click ...', async () => {
+        const options = {
+            propsData: {
+                options: {
+                    data: [
+                        { name: 'cjj', age: 18 },
+                        { name: 'we', age: 32 },
+                        { name: 'r', age: 43 },
+                        { name: 'ew', age: 34 },
+                        { name: 'fds', age: 5 },
+                        { name: 'd', age: 43 },
+                        { name: 'f', age: 5 },
+                        { name: 'fzf', age: 23 },
+                    ],
+                    tableColumn: [
+                        { label: '名字', index: 'name' },
+                        { label: '年龄', index: 'age' , render: col => '' + col.label },
+                    ],
+                    local: true,
+                    limit: 2,
+                    showPageColNum: 2,
+                },
+            },
+        }
+        const wrapper = TableMount(options)
+        const ellipes = wrapper.find('li.pagine-item:not(.page-index)')
+        await ellipes.trigger('click')
+        expect(ellipes.isVisible()).toBe(true)
+    })
+
 })
